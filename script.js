9021e240894460f4f1db377ebd809598e2b735da
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-07-22 14:09:48 -0400

    should be a phot with most choices.  I'm working on creating a default image if none are available

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ function getPokemon(nameOrId) {
   }
   
 function getRandomPokemon() {
-    const maxPokemonId = 1008; //some still don't have photos
+    const maxPokemonId = 1008;
     const randomId = Math.floor(Math.random() * maxPokemonId) + 1;
     getPokemon(randomId);
   }
   
 function updatePage(data) {
     document.getElementById('pokemonName').textContent = data.name;
-    document.getElementById('pokemonImage').src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/' + data.id + '.svg';
+    if (data.sprites.other.dream_world.front_default) {
+      document.getElementById('pokemonImage').src = data.sprites.other.dream_world.front_default;
+  } else if (data.sprites.front_default) {
+      document.getElementById('pokemonImage').src = data.sprites.front_default;
+  } else {
+      document.getElementById('pokemonImage').src = 'path/to/default/image.png';
+  }
+
     document.getElementById('pokemonBaseExperience').textContent = 'Base Experience: ' + data.base_experience;
     document.getElementById('pokemonHeight').textContent = 'Height: ' + data.height;
     document.getElementById('pokemonOrder').textContent = 'Order: ' + data.order;
     document.getElementById('pokemonWeight').textContent = 'Weight: ' + data.weight;
     document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + data.abilities.map(ability => ability.ability.name).join(', ');
+    let pokemonName = data.name.toUpperCase();
+    document.getElementById('pokemonName').textContent = pokemonName;
+    let pokemonAbilities = data.abilities.map(ability => ability.ability.name.toUpperCase()).join(', ');
+    document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + pokemonAbilities;
+
   }
 
   getPokemon('pikachu');
