8f4a0cbb0f45dcf3d0dab1643024940056b83cbe
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-07-26 14:20:31 -0400

    just doing a little font change

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
-
 function getPokemon(nameOrId) {
-    fetch('https://pokeapi.co/api/v2/pokemon/' + nameOrId)
-      .then(response => response.json())
-      .then(data => updatePage(data))
-      .catch(error => console.error('Error:', error));
-      document.getElementById('pokemonInput').value = '';
+  fetch('https://pokeapi.co/api/v2/pokemon/' + nameOrId)
+    .then(response => response.json())
+    .then(data => updatePage(data))
+    .catch(error => console.error('Error:', error));
+  document.getElementById('pokemonInput').value = '';
+}
+
+document.getElementById('pokemonInput').addEventListener('keypress', function(e) {
+  if (e.key === 'Enter') {
+    getPokemon(document.getElementById('pokemonInput').value);
   }
+});
 
-document.getElementById('pokemonInput').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        getPokemon(document.getElementById('pokemonInput').value);
-    }
-});  
-
-document.getElementById('pokemonImage').addEventListener('mouseenter', function () {
+document.getElementById('pokemonImage').addEventListener('mouseenter', function() {
   this.classList.add('move');
 
   // Remove the 'move' class after 2 seconds
   setTimeout(() => {
-      this.classList.remove('move');
+    this.classList.remove('move');
   }, 2000);
 });
 
-  
+
 function getRandomPokemon() {
-    const maxPokemonId = 1008;
-    const randomId = Math.floor(Math.random() * maxPokemonId) + 1;
-    getPokemon(randomId);
-  }
-  
+  const maxPokemonId = 1008;
+  const randomId = Math.floor(Math.random() * maxPokemonId) + 1;
+  getPokemon(randomId);
+}
+
 function updatePage(data) {
-    document.getElementById('pokemonName').textContent = data.name;
-    if (data.sprites.other.dream_world.front_default) {
-      document.getElementById('pokemonImage').src = data.sprites.other.dream_world.front_default;
+  document.getElementById('pokemonName').textContent = data.name;
+  if (data.sprites.other.dream_world.front_default) {
+    document.getElementById('pokemonImage').src = data.sprites.other.dream_world.front_default;
   } else if (data.sprites.front_default) {
-      document.getElementById('pokemonImage').src = data.sprites.front_default;
+    document.getElementById('pokemonImage').src = data.sprites.front_default;
   } else {
-      document.getElementById('pokemonImage').src = 'path/to/default/image.png';
+    document.getElementById('pokemonImage').src = 'path/to/default/image.png';
   }
 
-    document.getElementById('pokemonBaseExperience').textContent = 'Base Experience: ' + data.base_experience;
-    document.getElementById('pokemonHeight').textContent = 'Height: ' + data.height;
-    document.getElementById('pokemonOrder').textContent = 'Order: ' + data.order;
-    document.getElementById('pokemonWeight').textContent = 'Weight: ' + data.weight;
-    document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + data.abilities.map(ability => ability.ability.name).join(', ');
-    let pokemonName = data.name.toUpperCase();
-    document.getElementById('pokemonName').textContent = pokemonName;
-    let pokemonAbilities = data.abilities.map(ability => ability.ability.name.toUpperCase()).join(', ');
-    document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + pokemonAbilities;
+  document.getElementById('pokemonBaseExperience').textContent = 'Base Experience: ' + data.base_experience;
+  document.getElementById('pokemonHeight').textContent = 'Height: ' + data.height;
+  document.getElementById('pokemonOrder').textContent = 'Order: ' + data.order;
+  document.getElementById('pokemonWeight').textContent = 'Weight: ' + data.weight;
+  document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + data.abilities.map(ability => ability.ability.name).join(', ');
+  let pokemonName = data.name.toUpperCase();
+  document.getElementById('pokemonName').textContent = pokemonName;
+  let pokemonAbilities = data.abilities.map(ability => ability.ability.name.toUpperCase()).join(', ');
+  document.getElementById('pokemonAbilities').textContent = 'Abilities: ' + pokemonAbilities;
 
-  }
+}
 
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=1200')
-    .then(response => response.json())
-    .then(data => {
-        const dataList = document.getElementById('pokemonNames');
-        for (let i = 0; i < data.results.length; i++) {
-            const option = document.createElement('option');
-            option.value = data.results[i].name;
-            dataList.appendChild(option);
-        }
-    });
+fetch('https://pokeapi.co/api/v2/pokemon?limit=1200')
+  .then(response => response.json())
+  .then(data => {
+    const dataList = document.getElementById('pokemonNames');
+    for (let i = 0; i < data.results.length; i++) {
+      const option = document.createElement('option');
+      option.value = data.results[i].name;
+      dataList.appendChild(option);
+    }
+  });
 
 
-  getPokemon('pikachu');
+getPokemon('pikachu');
